839f0cf50cdbbbb7e70365edb66c58565147e879
Jake Whatley <jacob.whatley91@gmail.com>
2015-11-05 01:22:42 -0500

    vendor: symlink to /system/vendor
    depends on https://gerrit.omnirom.org/#/c/15522/

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -2,3 +2,6 @@
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
 TARGET_KERNEL_CONFIG := angler_defconfig
+
+# Enable vendor image symlink
+BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
