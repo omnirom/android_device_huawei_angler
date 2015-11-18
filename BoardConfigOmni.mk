6df57ade4df1a723d7bce4ae64a00614a2649ae2
Abhisek Devkota <ciwrl@cyanogenmod.com>
2015-11-18 12:45:17 -0800

    Use TARGET_KEYMASTER_WAIT_FOR_QSEE

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -5,3 +5,6 @@ TARGET_KERNEL_CONFIG := angler_defconfig
 
 # Enable vendor image symlink
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
+
+# Keymaster - Wait for qseecom to load
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
