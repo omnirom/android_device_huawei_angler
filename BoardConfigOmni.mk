5e064f09e9d6d6a55034a637b081e6def5589e7d
Jake Whatley <jacob.whatley91@gmail.com>
2015-12-09 20:06:23 -0500

    angler: use device's audio_effects.conf

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -3,6 +3,9 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
 TARGET_KERNEL_CONFIG := angler_defconfig
 
+# Use device's audio_effects.conf
+TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
+
 # Enable vendor image symlink
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
 
