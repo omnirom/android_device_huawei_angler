5aaf6c03bb7b06ba818491884b9a7f5b89818b8a
mcl21014 <helgalo94@comcast.net>
2016-02-04 22:33:49 -0500

    Build 64 bit kernel.
    Change-Id: I7f14630c941275b1d8d7029d37608430793135ee

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -2,6 +2,10 @@
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
 TARGET_KERNEL_CONFIG := angler_defconfig
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_USES_UNCOMPRESSED_KERNEL := true
 
 # Use device's audio_effects.conf
 TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
