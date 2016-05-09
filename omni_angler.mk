dd862c143b585f41f7318dffc812745a11a4e382
Austin Quach <austinquachh@gmail.com>
2016-05-08 17:27:04 -0700

    Update fingerprint to MTC19T
    Change-Id: Ibb357b67f743331edc8c43fb6a756cec09fa487e

--- omni_angler.mk
+++ omni_angler.mk
@@ -29,5 +29,5 @@ PRODUCT_MANUFACTURER := HUAWEI
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="google/angler/angler:6.0.1/MHC19Q/2705526:user/release-keys" \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MHC19Q 2705526 release-keys"
+    BUILD_FINGERPRINT="google/angler/angler:6.0.1/MTC19T/2741993:user/release-keys" \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19T 2741993 release-keys"
