538f65fdc4e7131f3e9016dc7ebbece436d3f33f
Marko Man <darkobas@gmail.com>
2016-06-08 10:49:48 +0200

    update fingerprint to MTC19V
    Change-Id: Ibe2a215497474e7f416716c44739e7f435797733

--- omni_angler.mk
+++ omni_angler.mk
@@ -30,5 +30,6 @@ PRODUCT_MANUFACTURER := Huawei
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="google/angler/angler:6.0.1/MTC19T/2741993:user/release-keys" \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19T 2741993 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC19V/2862947:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19V 2862947 release-keys" \
+    BUILD_ID=MTC19V
