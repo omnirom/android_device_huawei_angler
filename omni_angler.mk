0bfea1ec1ed8c2b98fb02ce25099c366e7400861
darkobas <darkobas@gmail.com>
2016-07-07 09:08:22 +0200

    angler: update fingerprint to MTC19X
    update camera dependancy to r54
    
    Change-Id: Ibda7e5272760cdf291b905a9130d9e3c3f7124a8

--- omni_angler.mk
+++ omni_angler.mk
@@ -43,6 +43,6 @@ PRODUCT_MANUFACTURER := Huawei
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC19V/2862947:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19V 2862947 release-keys" \
-    BUILD_ID=MTC19V
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC19X/2960136:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19X 2960136 release-keys" \
+    BUILD_ID=MTC19X
