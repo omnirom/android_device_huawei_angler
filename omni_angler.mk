c02fc73987ead3fc6ae0a3e931030d79b3f44762
darkobas <darkobas@gmail.com>
2016-08-02 11:27:14 +0200

    angler: update build id to MTC20F
    Change-Id: I957293b7f4e2260bc2429cb5b9152c538a86a946

--- omni_angler.mk
+++ omni_angler.mk
@@ -43,6 +43,6 @@ PRODUCT_MANUFACTURER := Huawei
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC19X/2960136:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19X 2960136 release-keys" \
-    BUILD_ID=MTC19X
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC20F/3031278:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC20F 3031278 release-keys" \
+    BUILD_ID=MTC20F
