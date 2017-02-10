5c55b130119ebbb87163a4d6139e77cdbae05ce5
Marko Man <darkobas@gmail.com>
2017-02-10 14:50:21 +0100

    angler: update to N4F26O
    Change-Id: I94d5ab1621075c1baf8dbb61206295854f84ffc5

--- omni_angler.mk
+++ omni_angler.mk
@@ -63,6 +63,6 @@ PRODUCT_MANUFACTURER := Huawei
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="google/angler/angler:7.1.1/N4F26J/3549317:user/release-keys" \
-    PRIVATE_BUILD_DESC="angler-user 7.1.1 N4F26J 3549317 release-keys" \
-    BUILD_ID=N4F26J
+    BUILD_FINGERPRINT="google/angler/angler:7.1.1/N4F26O/3582057:user/release-keys" \
+    PRIVATE_BUILD_DESC="angler-user 7.1.1 N4F26O 3582057 release-keys" \
+    BUILD_ID=N4F26O
